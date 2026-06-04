3fca639fe526b72acd7f90b95cd35911ebba7753
greenjourney11 <vinitjani1512@gmail.com>
2026-06-04 19:42:11 +0100

    Final merged Fridge-Mobile project

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
-import fridgeRouter from "./fridge";
-import fridgeSimRouter from "./fridge-sim";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
-router.use(fridgeRouter);
-router.use(fridgeSimRouter);
 
 export default router;
